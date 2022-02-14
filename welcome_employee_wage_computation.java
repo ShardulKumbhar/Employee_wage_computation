b93136b92c16fc682fccc57055c9ee6e9114bf70
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-14 23:04:33 +0530

    added welcome file

---
+++ welcome_employee_wage_computation.java
@@ -0,0 +1,10 @@
+package welcome;
+
+public class welcome_employee_wage_computation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage computation");
+
+	}
+
+}
